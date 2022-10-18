1f6013146e17ce63307b9729d35348174e562a27
Le Gloux <legloux.thomas@gmail.com>
2022-10-18 12:40:03 +0200

    optimize vue (dwelling per category, errors pages made

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,18 +2,19 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/category', name: 'app_category')]
+#[Route('/category', name: 'app_category_')]
 class CategoryController extends AbstractController
 {
-    #[Route('/{name}', name: 'app_category_name')]
-    public function index(): Response
+    #[Route('/{name}', name: 'name')]
+    public function index(Category $category): Response
     {
-        return $this->render('category/index.html.twig', [
-           
-        ]);
+        $dwellings= $category->getDwellings();
+        // on va chercher la liste des habitations de la catégorie
+        return $this->render('category/index.html.twig', compact('category', 'dwellings'));
     }
 }
