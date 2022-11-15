da9dad50d053ce8daf095213970221d8d36a8de1
Le Gloux <legloux.thomas@gmail.com>
2022-11-15 11:46:49 +0100

    improve front-end : added a dwelling page details

--- src/Controller/DwellingController.php
+++ src/Controller/DwellingController.php
@@ -2,17 +2,16 @@
 
 namespace App\Controller;
 
+use App\Entity\Dwelling;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DwellingController extends AbstractController
 {
-    #[Route('/dwelling', name: 'app_dwelling')]
-    public function index(): Response
+    #[Route('/{name}', name: 'dwelling_details')]
+    public function details(Dwelling $dwelling): Response
     {
-        return $this->render('dwelling/index.html.twig', [
-            'controller_name' => 'DwellingController',
-        ]);
+        return $this->render('dwelling/index.html.twig', compact('dwelling'));
     }
 }
