6fbe76381ed6f43e19493b967cb87e3124e4bda7
Le Gloux <legloux.thomas@gmail.com>
2022-11-29 16:09:08 +0100

    some improvements

--- src/Entity/Dwelling.php
+++ src/Entity/Dwelling.php
@@ -7,6 +7,8 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use App\Service\UploaderHelper;
+use UploaderHelper as GlobalUploaderHelper;
 
 #[ORM\Entity(repositoryClass: DwellingRepository::class)]
 class Dwelling
@@ -210,5 +212,4 @@ class Dwelling
 
         return $this;
     }
-
 }
--- src/Form/AdminAddDwellingFormType.php
+++ src/Form/AdminAddDwellingFormType.php
@@ -2,9 +2,13 @@
 
 namespace App\Form;
 
+use App\Entity\Category;
 use App\Entity\Dwelling;
+use App\Entity\Type;
 use FOS\CKEditorBundle\Form\Type\CKEditorType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -46,33 +50,56 @@ class AdminAddDwellingFormType extends AbstractType
                     "class" => "form-control"
                 ]
             ])
-            ->add('price',TextType::class, [
+            ->add('price', TextType::class, [
                 'attr' =>
                 [
                     "class" => "form-control"
                 ]
             ])
-            ->add('description',CKEditorType::class, [
+            ->add('description', CKEditorType::class, [
                 'attr' =>
                 [
                     "class" => "form-control"
                 ]
-            ])
-            ->add('type')
-            ->add('category')
-            ->add('picture', FileType::class,[
-                'data_class'=>null
             ])
             ->add(
-                'Envoyer',
+                'type',
+                EntityType::class,
+                [
+                    'class' => Type::class,
+                    'choice_label' => 'name',
+                    'attr' => [
+                        "class" => "form-select"
+                    ]
+                ]
+            )
+            ->add(
+                'category',
+                EntityType::class,
+                [
+                    'class' => Category::class,
+                    'choice_label' => 'name',
+                    'attr' => [
+                        "class" => "form-select"
+                    ]
+                ]
+            )
+            ->add('picture', FileType::class, [
+                'data_class' => null,
+                'attr' =>
+                [
+                    "class" => "form-control"
+                ]
+            ])
+            ->add(
+                'Valider',
                 SubmitType::class,
                 [
                     'attr' => [
                         'class' => 'btn formBtn'
                     ]
                 ]
-            )
-        ;
+            );
     }
 
     public function configureOptions(OptionsResolver $resolver): void
